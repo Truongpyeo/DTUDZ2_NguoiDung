4bc20465cb7b66c4b2f4a1e5dfad82203ccf0be9
vietvo371 <vietvo371@gmail.com>
2024-12-05 12:22:06 +0000

    fix page yêu cầu khẩn cấp

--- pages/Trang Chủ/jsobjects/JSObject1/JSObject1.js
+++ pages/Trang Chủ/jsobjects/JSObject1/JSObject1.js
@@ -3,7 +3,16 @@ export default {
 
 	setDefaultTab: (newTab) => {
 		this.defaultTab = newTab;
-		FilePicker1.files
 	},
-	
+	actionYeuCauTroCap: () => {
+		const check = appsmith.store.id_user;
+		if(check){
+			navigateTo("Yêu cầu cứu trợ")
+		}
+		else{
+			showAlert("Vui lòng đăng nhập để yêu cầu cứu trợ!" , "error");
+			navigateTo("Đăng Nhập")
+		}
+	}
+
 }
